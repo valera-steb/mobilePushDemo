0961dec820793cf91394866134d78633e292a02d
valera-steb <valera.steb@yandex.ua>
2019-11-18 16:58:21 +0300

    all files

---
+++ FmcPush/FmcPush/LogItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FmcPush
+{
+    public class LogItem
+    {
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+    }
+}
